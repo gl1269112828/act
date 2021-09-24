3d6011e6c2b61d3ef35f7f8cfe4e72efa2162e1f
phil <SkipperGuo@outlook.com>
2021-09-24 14:53:40 +0800

    修改自定义油价信息宽度

--- src/views/oil/priceJson.vue
+++ src/views/oil/priceJson.vue
@@ -1,16 +1,16 @@
 <template>
   <el-table-column :prop="tableHeaderData.prop" :label="tableHeaderData.label" :width="tableHeaderData.width" align="center">
-    <el-table-column label="92#" align="center">
+    <el-table-column label="92#" align="center" width="100">
       <template slot-scope="scope">
         <span>{{ JSON.parse(scope.row.priceJson)[0].price || '' }}</span>
       </template>
     </el-table-column>
-    <el-table-column label="95#" align="center">
+    <el-table-column label="95#" align="center" width="100">
       <template slot-scope="scope">
         <span>{{ JSON.parse(scope.row.priceJson)[1].price || '' }}</span>
       </template>
     </el-table-column>
-    <el-table-column label="98#" align="center">
+    <el-table-column label="98#" align="center" width="100">
       <template slot-scope="scope">
         <span>{{ JSON.parse(scope.row.priceJson)[2].price || '' }}</span>
       </template>
